904dd2f2943d7bd631d864e7b62a442168862a96
mdbow22 <mdbow22@gmail.com>
2021-12-13 22:14:40 -0500

    updated CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Landing from './components/Landing';
 
 function App() {
+
+  useEffect(() => {
+    document.body.classList.add('bg-gray-900', 'text-cyan-50')
+  }, [])
+
   return (
-    <div className="max-w-screen-lg mx-auto ">
+    <div className="max-w-screen-lg mx-auto min-h-screen">
       <Header />
       <main>
         <Landing />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,9 +12,10 @@ function Navigation() {
                 </div>
                 <div id="menu-list" className="hidden sm:block bg-gray-700 absolute top-16 w-full sm:relative sm:w-auto sm:top-0">
                     <ul id="menu-items" className="w-full sm:w-auto h-full flex sm:relative sm:top-0 flex-col sm:flex-row items-stretch justify-center font-semibold">
-                        <li className="border-b border-gray-500 sm:border-0 p-3 px-6 card-link"><a class="inline-block h-full w-full" href="#about">About</a></li>
-                        <li className="border-b border-gray-500 sm:border-0  p-3 px-6 card-link"><a class="inline-block h-full w-full"  href="#projects">Projects</a></li>
-                        <li className="border-b border-gray-500 sm:border-0  p-3 px-6 card-link"><a class="inline-block h-full w-full"  href="#connect">Connect</a></li>
+                        <li className="menu-list-item card-link"><a class="inline-block h-full w-full" href="#">Home</a></li>
+                        <li className="menu-list-item card-link"><a class="inline-block h-full w-full" href="#about">About</a></li>
+                        <li className="menu-list-item card-link"><a class="inline-block h-full w-full"  href="#projects">Projects</a></li>
+                        <li className="menu-list-item card-link"><a class="inline-block h-full w-full"  href="#connect">Connect</a></li>
                     </ul>
                 </div>
                 
